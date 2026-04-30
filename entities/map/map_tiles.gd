c34c012081c53c9daed0cebb6deb727214371af1
Jared Royer <jarroyer@me.com>
2026-04-29 20:51:07 -0500

    Adding cell alternatives

--- entities/map/map_tiles.gd
+++ entities/map/map_tiles.gd
@@ -1,5 +1,11 @@
 extends TileMapLayer
 
+var HALLWAY = Vector2i(0, 0);
+
 func _ready():
-	print("ready")
-	print(get_children())
+	set_cell(Vector2i(0, 0), 0, HALLWAY)
+	set_cell(Vector2i(1, 0), 0, Vector2i(3, 0))
+	var cell_to_flip = get_cell_tile_data(Vector2i(0, 0))
+	print(cell_to_flip)
+	cell_to_flip.transpose = true;
+	cell_to_flip.flip_h = true;
