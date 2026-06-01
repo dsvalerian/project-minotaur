5665c9a5dd79005a36fffb2b4a96f48104984e55
Jared Royer <jarroyer@me.com>
2026-05-31 21:08:55 -0500

    Adding character select screen, converting characters to using resources

--- features/characters/character.gd
+++ features/characters/character.gd
@@ -1,28 +1,12 @@
 class_name Character extends Node2D 
 
-@export var title: String = "John Character"
-
-@export_group("Stats")
-@export var max_health: int = 10
-@export var initiative: int = 5
-@export var speed: int = 2
-
-func get_portrait() -> Container:
-	var texture = $CharacterSprite.texture
-	var rect = TextureRect.new()
-	rect.texture = texture
-	rect.expand_mode = TextureRect.EXPAND_FIT_WIDTH
-	rect.stretch_mode = TextureRect.STRETCH_SCALE
-	rect.custom_minimum_size = Vector2(32, 32)
-	var ratio = AspectRatioContainer.new()
-	ratio.add_child(rect)
-	ratio.ratio = float(9) / float(16);
-	return ratio 
-
-func get_sprite() -> Sprite2D:
-	return $CharacterSprite
+@export var character_name: String
+@export var character_class: CharacterClass
+@export var character_stats: Stats
 
 func _ready() -> void:
+	character_stats = character_class.base_stats
+	$CharacterSprite.texture = character_class.icon
 	Signals.character_spawned.emit(self)
 
 func die() -> void:
--- features/game_manager/gameplay/gameplay_manager.gd
+++ features/game_manager/gameplay/gameplay_manager.gd
@@ -1,25 +1,4 @@
 extends Node2D
 
 @onready var map_manager: MapManager = $MapManager
-
-func _ready() -> void:
-	#var fighter_scene = CharacterFactory.getCharacterScene(CharacterFactory.CharacterClass.FIGHTER)
-	#var priest_scene = CharacterFactory.getCharacterScene(CharacterFactory.CharacterClass.PRIEST)
-	#var rogue_scene = CharacterFactory.getCharacterScene(CharacterFactory.CharacterClass.ROGUE)
-	#var wizard_scene = CharacterFactory.getCharacterScene(CharacterFactory.CharacterClass.WIZARD)
-	#var fighter = fighter_scene.instantiate()
-	#var priest = priest_scene.instantiate()
-	#var rogue = rogue_scene.instantiate()
-	#var wizard = wizard_scene.instantiate()
-	#map_manager.add_child(fighter)
-	##map_manager.add_child(priest)
-	#map_manager.add_child(rogue)
-	#map_manager.add_child(wizard)
-	_make_fighter(10)
-
-func _make_fighter(num_fighters: int):
-	for i in range(num_fighters):
-		var fighter_scene = CharacterFactory.getCharacterScene(CharacterFactory.CharacterClass.FIGHTER)
-		var fighter = fighter_scene.instantiate()
-		map_manager.add_child(fighter)
 	
